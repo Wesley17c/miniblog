78969ac7696d9676c8fec1d0f6eda1d3699460cf
wesley soares <ws17c@outlook.com>
2023-09-19 14:56:49 -0300

    init

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5eRqINjLw0KROEKcIBVwwn7iM2u2aSMI",
+  authDomain: "miniblog-aede5.firebaseapp.com",
+  projectId: "miniblog-aede5",
+  storageBucket: "miniblog-aede5.appspot.com",
+  messagingSenderId: "605827582408",
+  appId: "1:605827582408:web:9eca8ddcb773d73d5d98fd"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
