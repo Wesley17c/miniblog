bc1d4cfaf8e20a9bc9837e16f5ee8f4622b11bce
wesley soares <ws17c@outlook.com>
2023-09-19 17:43:39 -0300

    navbar e footer concluídos

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+            <h3>escreva sobre o que você tem interesse!</h3>
+            <p> MiniBlog &copy; feito em 2023 </p>
+    </footer>
+  )
+}
+
+export default Footer;
