05a5256fea5c64e527153819a3e13e3f3fa99f36
wesley soares <ws17c@outlook.com>
2023-09-19 15:57:02 -0300

    pages init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
+
+//PAGES
+
+
+import About from './pages/About/About';
+import Home from './pages/About/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
       
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+      
+         <div className="container">
+             <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/about' element={<About/>}/>
+            </Routes>
+         </div>
+       </BrowserRouter>
+
     </div>
   );
 }
