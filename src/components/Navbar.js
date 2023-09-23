6d079ddd49706d7f8005e22fd17edc32a13a4165
wesley soares <ws17c@outlokk.com>
2023-09-23 14:41:07 -0300

    diferença de exibir icones /user auth ou no auth/

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,47 @@
 import { NavLink } from "react-router-dom";
 import styles from "../components/NavBar.module.css";
+import { useAuthentication } from "../useAuthentication";
+import { useAuthValue } from "../Context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useAuthValue();
+
   return (
     <nav className={styles.navbar}>
-
       <NavLink to={"/"} className={styles.blog}>
         Mini <span>Blog</span>
       </NavLink>
 
       <ul className={styles.list}>
         <li>
-             <NavLink to={"/"}>Home</NavLink>
-        </li>
-        <li>
-             <NavLink to={"/login"} >Entrar</NavLink>
-        </li>
-        <li>
-             <NavLink to={"/register"} >Cadastrar</NavLink>
-        </li>
-        <li>
-             <NavLink to={"/about"} >Sobre</NavLink>
+          <NavLink to={"/"}>Home</NavLink>
         </li>
 
+        {!user && (
+          <>
+            <li>
+              <NavLink to={"/login"}>Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/register"}>Cadastrar</NavLink>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>
+              <NavLink to={"/post/create"}> Novo post</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/dashboard"}>Dashboard</NavLink>
+            </li>
+          </>
+        )}
+
+        <li>
+          <NavLink to={"/about"}>Sobre</NavLink>
+        </li>
       </ul>
     </nav>
   );
