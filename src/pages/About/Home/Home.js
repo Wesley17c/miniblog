b6872af7fbfc3a6879983d5e65d47ee4d8269203
wesley soares <ws17c@outlook.com>
2023-09-26 15:11:17 -0300

    finalização da estrutura e css da home

--- src/pages/About/Home/Home.js
+++ src/pages/About/Home/Home.js
@@ -1,11 +1,54 @@
-import styles from '../Home/Home.module.css';
+import styles from "../Home/Home.module.css";
+
+// HOOKS
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
+
+  const [busca, setBusca] = useState("");
+  const [posts] = useState([]);
+  const navigate = useNavigate();
+
+  navigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-        <h1 className={styles.h1} >Homepage</h1>
+    <div className={styles.wrapper_container}>
+      <h1 className={styles.h1}>Veja os nossos posts mais recentes</h1>
+
+      <form onSubmit={handleSubmit} className={styles.formulario}> 
+
+        <label className={styles.forms}>
+          <input
+            type="text"
+            placeholder="pesquise por tags"
+            onChange={(e) => setBusca(e.target.value)}
+            value={busca}
+          />
+        </label>
+
+        <button className={styles.btn}> buscar </button>
+        
+      </form>
+
+      <div className={styles.wrapper_container}>
+        
+      <h2>posts</h2>
+
+        {posts && posts.length === 0 && (
+          <div className={styles.list}>
+            <p>Não foram encontrados posts...</p>
+            <Link to={'/post/create'}> Clique aqui para criar</Link>
+          </div>
+        )}
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default Home;
