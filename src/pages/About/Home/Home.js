3c2b17521906f8e0ed02879eb91876d7d02183ef
wesley soares <ws17c@outlook.com>
2023-10-02 13:28:35 -0300

    css

--- src/pages/About/Home/Home.js
+++ src/pages/About/Home/Home.js
@@ -2,7 +2,7 @@ import styles from "../Home/Home.module.css";
 
 // HOOKS
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useFetchDocuments } from "../../../Hooks/useFetchDocuments";
 //COMPONENTS
 import PostDetails from "../../../components/PostDetails";
@@ -10,6 +10,18 @@ import PostDetails from "../../../components/PostDetails";
 const Home = () => {
   const [busca, setBusca] = useState("");
   const { documents: posts, loading } = useFetchDocuments("post");
+
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    // Adiciona a classe 'scaled' após um pequeno atraso para ativar a transição
+    const timer = setTimeout(() => {
+      setAnimate(true);
+    }, 300);
+    
+    // Limpa o timer para evitar efeitos colaterais se o componente for desmontado antes do timer expirar
+    return () => clearTimeout(timer);
+  }, []); // Executa apenas uma vez após a montagem inicial
 
   
   const navigate = useNavigate();
@@ -28,6 +40,13 @@ const Home = () => {
   
   return (
     <div className={styles.wrapper_container}>
+
+      {/* se a animação for verdadeira, adicione a classe scaled */}
+      <div className={`${styles.texto} ${animate ? styles.scaled : ""}`}>
+        <h2>Seja bem vindo!</h2>
+      </div>
+
+
       <h1 className={styles.h1}>Veja os nossos posts mais recentes</h1>
 
       <form onSubmit={handleSubmit} className={styles.formulario}>
